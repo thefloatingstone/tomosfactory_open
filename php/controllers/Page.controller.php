598bebe2490c6a010302fdc0e74de221fa4cf07b
greggythefly <gregoire@fournel.net>
2013-05-28 23:38:35 +0200

    adding Controllers

---
+++ php/controllers/Page.controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class PageController extends ObjectController {
+    
+    public function create() {
+        
+    }
+    
+    public function save() {
+        
+    }
+    
+    public function delete() {
+        
+    }
+
+}
---
+++ php/controllers/Tomos.controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class TomosController extends ObjectController {
+    
+    public function create() {
+        
+    }
+    
+    public function save() {
+        
+    }
+    
+    public function delete() {
+        
+    }
+
+}
