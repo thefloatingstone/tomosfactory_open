3d8d2442016799337da843a27e291b9c23938709
greggythefly <gregoire@fournel.net>
2013-05-28 22:16:02 +0200

    adding Smarty, Bootstrap, jQuery ...

---
+++ php/init.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+function __autoload($class_name) {
+    if(file_exists('classes/'.$class_name . '.class.php')) {
+        require_once('classes/'.$class_name . '.class.php');
+    } else {
+        throw new Exception("Unable to load $class_name.");
+    }
+}
