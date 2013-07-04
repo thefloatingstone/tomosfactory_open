08209e61bc71a8e09016ccf032a25d18e651d380
greggythefly <gregoire@fournel.net>
2013-07-04 11:24:56 +0200

    adding jquery 1.10.1

--- _tf_/php/classes/Database.class.php
+++ _tf_/php/classes/Database.class.php
@@ -1,9 +1,43 @@
 <?php
 
 class Database {
-    
-    public static function getPDO() {
-        return new PDO("mysql:host=" . TOMOSFACTORY_DATABASE_HOST . ";dbname=" . TOMOSFACTORY_DATABASE_NAME, TOMOSFACTORY_DATABASE_USER, TOMOSFACTORY_DATABASE_PASSWORD);
+
+    /**
+     * @var Database
+     * @access private
+     * @static
+     */
+    private static $_instance = null;
+    private $pdo;
+
+    /**
+     * Constructeur de la classe
+     *
+     * @param void
+     * @return void
+     */
+    private function __construct() {
+        $this->pdo = new PDO(DATABASE_DRIVER.":host=" . DATABASE_HOST . ";dbname=" . DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD);
     }
-    
+
+    /**
+     * Méthode qui crée l'unique instance de la classe
+     * si elle n'existe pas encore puis la retourne.
+     *
+     * @param void
+     * @return Singleton
+     */
+    public static function getInstance() {
+
+        if (is_null(self::$_instance)) {
+            self::$_instance = new Database();
+        }
+
+        return self::$_instance;
+    }
+
+    public function getPDO() {
+        return $this->pdo;
+    }
+
 }
